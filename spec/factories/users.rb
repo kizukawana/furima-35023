b55755fe774f9a04667136e31e1c6f8d05c5899c
kizukawana <75799522+kizukawana@users.noreply.github.com>
2021-04-13 19:36:12 +0900

    修正5

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :user do
     nickname              {'test'}
-    email                 {'test@example'}
+    email                 {Faker::Internet.free_email}
     password              {'a123456'}
     password_confirmation {'a123456'}
     first_name_kanji      {'あア阿'}
