acb90da6f688ffa05fa70cff051597f3c88adb3a
kizukawana <75799522+kizukawana@users.noreply.github.com>
2021-04-19 16:05:29 +0900

    商品情報編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except:[:index, :show]
+  before_action :move_to_index, except: [:index, :show, :require_login]
+  before_action :set_item, only: [:show, :edit, :update]
+
   def index
     @items = Item.order("id ASC")
   end
@@ -18,8 +20,23 @@ class ItemsController < ApplicationController
   end
  
   def show
-    @item = Item.find(params[:id])
+     @item = Item.find(params[:id])
+  end
 
+  def edit
+     @item = Item.find(params[:id]) 
+  end
+
+  def update
+     @item = Item.find(params[:id]) 
+    if @item.update(item_params) 
+      redirect_to item_path 
+    else
+      render :edit
+    end
+  end
+
+  def require_login
   end
 
   private
@@ -27,4 +44,13 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:image, :name, :explanation, :category_id, :condition_id, :delivery_fee_id, :area_id, :delivery_day_id, :price ).merge(user_id: current_user.id)
   end
+
+  def move_to_index
+    redirect_to root_path unless user_signed_in?
+  end
+
+  def set_item
+    @items = Item.find(params[:id])
+  end
 end
+
